ac5bbaca076e93b1344a904b596b51b018239eb6
Azka Andya <azkaandyaaa@gmail.com>
2020-05-10 16:20:58 +0700

    first

--- script/topanime.js
+++ script/topanime.js
@@ -1,4 +1,5 @@
 const base_url = "https://api.jikan.moe/v3";
+const end_point = "https://api.jikan.moe/v3/top/"
 
 
 function requestTopAnime(url, onComplete, onError) {
@@ -10,4 +11,15 @@ function requestTopAnime(url, onComplete, onError) {
         .then(updateDom)
         .catch(err=>console.warn(err.message));
 }
+
+function generateAnimeUrl(path) {
+    const url = `${MOVIE_DB_ENDPOINT}anime?q=favorite`;
+    return url;
+}
+function getTopRatedMovies() {
+    const url = generateAnimeUrl;
+    const render = renderMovies.bind({ title: 'Top Rated Movies' })
+    requestTopAnime(url, render, handleGeneralError);
+}
+
 
